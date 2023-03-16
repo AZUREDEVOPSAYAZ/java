a7ecd6224102bec980338c57c6bb4e8720065fdb
AZUREDEVOPSAYAZ <122391893+AZUREDEVOPSAYAZ@users.noreply.github.com>
2023-03-16 09:31:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Java Jenkin  *****-----");
+		System.out.println("-----***** Java   *****-----");
 	}
 }

7f2918eccafa47cf9abbaafd6edacb66ddfacd67
AZUREDEVOPSAYAZ <122391893+AZUREDEVOPSAYAZ@users.noreply.github.com>
2023-03-16 10:33:58 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Java project -0001   *****-----");
+		System.out.println("-----***** Java project -0001   webhook  *****-----");
 	}
 }
